92fd88ab918cf20b094c70ebc6d3955679cc7f2a
Austin Pray <austin@austinpray.com>
2020-07-26 14:06:56 -0500

    Use DidYouMean::Levenshtein in Gem::Text
    GEM::Text.levenshtein_distance is now an alias for DidYouMean::Levenshtein.distance
    
    - Significant performance boost
    - Benefits from upstream bugfixes and future performance enhancements

--- lib/rubygems/text.rb
+++ lib/rubygems/text.rb
@@ -52,34 +52,6 @@ module Gem::Text
   # This code is based directly on the Text gem implementation
   # Returns a value representing the "cost" of transforming str1 into str2
   def levenshtein_distance(str1, str2)
-    s = str1
-    t = str2
-    n = s.length
-    m = t.length
-
-    return m if (0 == n)
-    return n if (0 == m)
-
-    d = (0..m).to_a
-    x = nil
-
-    str1.each_char.each_with_index do |char1,i|
-      e = i + 1
-
-      str2.each_char.each_with_index do |char2,j|
-        cost = (char1 == char2) ? 0 : 1
-        x = min3(
-             d[j + 1] + 1, # insertion
-             e + 1,      # deletion
-             d[j] + cost # substitution
-           )
-        d[j] = e
-        e = x
-      end
-
-      d[m] = x
-    end
-
-    return x
+    DidYouMean::Levenshtein.distance(str1, str2)
   end
 end
