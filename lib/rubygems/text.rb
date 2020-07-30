63f39616e4bd05bfe9dd8eeb7e39e90113062a81
Austin Pray <austin@austinpray.com>
2020-07-30 00:06:09 -0500

    Backwards compat for people without DidYouMean

--- lib/rubygems/text.rb
+++ lib/rubygems/text.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'did_you_mean/levenshtein'
-
 ##
 # A collection of text-wrangling methods
 
@@ -51,9 +49,54 @@ module Gem::Text
     end
   end
 
-  # This code is based directly on the Text gem implementation
   # Returns a value representing the "cost" of transforming str1 into str2
+  # Vendored version of DidYouMean::Levenshtein.distance from the did_you_mean gem
+  # https://git.io/JJgZI
   def levenshtein_distance(str1, str2)
-    DidYouMean::Levenshtein.distance(str1, str2)
+    n = str1.length
+    m = str2.length
+    return m if n.zero?
+    return n if m.zero?
+
+    d = (0..m).to_a
+    x = nil
+
+    # to avoid duplicating an enumerable object, create it outside of the loop
+    str2_codepoints = str2.codepoints
+
+    str1.each_codepoint.with_index(1) do |char1, i|
+      j = 0
+      while j < m
+        cost = (char1 == str2_codepoints[j]) ? 0 : 1
+        x = min3(
+          d[j + 1] + 1, # insertion
+          i + 1,      # deletion
+          d[j] + cost # substitution
+        )
+        d[j] = i
+        i = x
+
+        j += 1
+      end
+      d[m] = x
+    end
+
+    x
   end
+end
+
+# Use the latest and greatest DidYouMean::Levenshtein if it is available
+begin
+  require 'did_you_mean/levenshtein'
+
+  module Gem::Text::LevenshteinExt
+    def levenshtein_distance(str1, str2)
+      DidYouMean::Levenshtein.distance(str1, str2)
+    end
+  end
+
+  Gem::Text.prepend(Gem::Text::LevenshteinExt)
+
+rescue LoadError
+  # ignored
 end
